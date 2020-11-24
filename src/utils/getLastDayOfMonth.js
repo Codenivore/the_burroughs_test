bdc3e45f4768a15770ea9c42cff475b5290070d1
Codenivore <nick.mcalpine@gmail.com>
2020-11-24 16:51:50 +0000

    func to get last day of the month

--- src/utils/getLastDayOfMonth.js
+++ src/utils/getLastDayOfMonth.js
@@ -1,2 +1,17 @@
-const getLastDayOfMonth = () => {}
+const IsValidDate = require('./isValidDateObject')
+
+const getLastDayOfMonth = (argDate) => {
+  // if not a valid date not point going further so return out
+  if (!IsValidDate(argDate)) {
+    return 'Invalid input'
+  }
+
+  const monthofArgDate = argDate.getMonth()
+  const yearOFArgDate = argDate.getFullYear()
+
+  const lastDayDate = new Date(yearOFArgDate, monthofArgDate + 1, 0)
+  const lastDay = lastDayDate.getDate()
+
+  return lastDay
+}
 module.exports = getLastDayOfMonth
