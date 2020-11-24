ff2dd9191d62ed0f44bcc1a7f4c580e271745e64
Codenivore <nick.mcalpine@gmail.com>
2020-11-24 16:17:21 +0000

    function to get day as int which passes tests

--- src/utils/getDayInt.js
+++ src/utils/getDayInt.js
@@ -1,2 +1,13 @@
-const getDayInt = () => {}
+const IsValidDate = require('./isValidDateObject')
+
+const getDayInt = (argDate) => {
+  // if not a valid date not point going further so return out
+  if (!IsValidDate(argDate)) {
+    return 'Invalid input'
+  }
+
+  const dayAsInt = argDate.getDay()
+
+  return dayAsInt
+}
 module.exports = getDayInt
